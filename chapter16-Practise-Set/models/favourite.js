3da69a197e06696ebfbc18c86e4a964d8a38e550
Rajan kumar <rk2762992@gmail.com>
2025-08-30 22:12:44 -0700

    chapter16 practise set

---
+++ chapter16-Practise-Set/models/favourite.js
@@ -0,0 +1,39 @@
+const { getDB } = require("../utils/databaseUtil");
+
+ 
+
+
+
+
+module.exports = class Favourite {
+
+   constructor(houseId) {
+    this.houseId = houseId;
+ }
+
+ save(){
+  const db = getDB();
+   return db.collection('Favourites').findOne({houseId: this.houseId}).then(existingFav => {
+      
+      if (!existingFav) {
+      return db.collection('Favourites').insertOne(this);
+   }
+   return  Promise.resolve();
+  })
+  
+ }
+
+
+ static getFavourites() {
+     const db = getDB();
+  return db.collection('Favourites').find().toArray();
+ 
+ }
+
+
+ static deleteById(delHomeId) {
+       const db = getDB();
+    return db.collection('Favourites')
+    .deleteOne({houseId:delHomeId });
+ }
+}

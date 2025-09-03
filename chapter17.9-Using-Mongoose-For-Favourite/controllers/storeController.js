c42a750c419de7dec2989d3fede3b9a38ea253ed
Rajan kumar <rk2762992@gmail.com>
2025-09-03 19:02:57 +0530

    17.9 Using Mongoose for Favourite

---
+++ chapter17.9-Using-Mongoose-For-Favourite/controllers/storeController.js
@@ -0,0 +1,94 @@
+const Favourite = require("../models/favourite");
+const Home = require("../models/home");
+exports.getIndex = (req, res, next) => {
+  Home.find().then((registeredHomes) => {
+    res.render("store/index", {
+      registeredHomes: registeredHomes,
+      pageTitle: "airbnb HOme",
+      currentPage: "index",
+    });
+  });
+};
+
+exports.getHomes = (req, res, next) => {
+  Home.find().then((registeredHomes) => {
+    res.render("store/home-list", {
+      registeredHomes: registeredHomes,
+      pageTitle: "Home List",
+      currentPage: "home",
+    });
+  });
+};
+exports.getBookings = (req, res, next) => {
+  res.render("store/bookings", {
+    pageTitle: "My Bookings",
+    currentPage: "bookings",
+  });
+};
+
+exports.getFavouriteList = (req, res, next) => {
+  Favourite.find().then((favourites) => {
+    Home.find().then((registeredHomes) => {
+      console.log(favourites, registeredHomes);
+      favourites = favourites.map((fav) => fav.houseId.toString());
+      const favouriteHomes = registeredHomes.filter((home) =>
+        favourites.includes(home._id.toString())
+      );
+      res.render("store/favourite-list", {
+        favouriteHomes: favouriteHomes,
+        pageTitle: "My Favourites",
+        currentPage: "favourites",
+      });
+    });
+  });
+};
+
+exports.postAddToFavourite = (req, res, next) => {
+  const houseId = req.body.id;
+  Favourite.findOne({ houseId: houseId })
+    .then((fav) => {
+      if (fav) {
+        console.log("Already marked as favourite");
+        res.redirect("/favourites");
+      } else {
+        fav = new Favourite({ houseId: houseId });
+        fav.save().then((result) => {
+          console.log("Fav added: ", result);
+        });
+        res.redirect("/favourites");
+      }
+    })
+    .catch((err) => {
+      console.log("Error while marking favourite:", err);
+    });
+};
+
+exports.postRemoveFromFavourite = (req, res, next) => {
+  const homeId = req.params.homeId;
+  Favourite.findOneAndDelete({ houseId: homeId })
+    .then((result) => {
+      console.log("Fav Removed: ", result);
+    })
+    .catch((err) => {
+      console.log("Error While marking favourite:", err);
+    })
+    .finally(() => {
+      res.redirect("/favourites");
+    });
+};
+
+exports.getHomeDetails = (req, res, next) => {
+  const homeId = req.params.homeId;
+  Home.findById(homeId).then((home) => {
+    if (!home) {
+      console.log("Home not found");
+      res.redirect("/homes");
+    } else {
+      res.render("store/home-detail", {
+        home: home,
+        pageTitle: "Home Details",
+        currentPage: "home",
+      });
+    }
+  });
+};

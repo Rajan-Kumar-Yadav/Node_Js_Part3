832e71f12c75e7361584d22db1f8a9e2d9570712
Rajan kumar <rk2762992@gmail.com>
2025-09-05 16:16:24 +0530

    18.2 Adding Login Functionality and 18.3 Checking Login State

---
+++ chapter18-Checking-Login-State/controllers/authController.js
@@ -0,0 +1,18 @@
+exports.getLogin = (req, res, next) => {
+  res.render("auth/login", {
+    pageTitle: "Login",
+    currentPage: "login",
+    isLoggedIn: false,
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  console.log(req.body);
+  res.cookie("isLoggedIn", true);
+  // req.isLoggedIn = true;
+  res.redirect("/");
+};
+exports.postLogout = (req, res, next) => {
+  res.cookie("isLoggedIn", false);
+  res.redirect("/login");
+};
